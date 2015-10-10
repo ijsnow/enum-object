f2a9e67dfaf731d1e822aa5d350b2f3431a89fa6
Isaac Snow <isaacjsnow@gmail.com>
2015-10-09 21:24:16 -0500

    Initial commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'ijsnow:enum-object',
+  version: '0.0.1',
+  summary: 'A simple enum object.',
+  git: 'https://github.com/ijsnow/enum-object',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('ecmascript');
+  api.addFiles('enum-object.js');
+
+  api.export('EnumObject');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('ijsnow:enum-object');
+  api.addFiles('enum-object-tests.js');
+});
